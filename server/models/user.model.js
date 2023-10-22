4656e14b84e9872281f58f25f4a9df6647ac7ebf
Sahib Semahegn <semahegnsahib@gmail.com>
2023-10-22 16:18:57 +0300

    fix error on user.model.js

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
@@ -18,7 +18,7 @@ const userSchema = mongoose.Schema(
     },
   },
   {
-    timestaps: true,
+    timestamps: true,
   }
 );
 
