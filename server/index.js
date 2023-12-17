f1abc9977c69b58908627be0765491fa86977428
sahib-sem <semahegnsahib@gmail.com>
2023-12-17 14:13:28 +0300

    sign-up route and controller

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js'
 
 dotenv.config();
 
@@ -13,8 +15,12 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 
 const app = express();
 
+
 const port = 3000
 
 app.listen(port, () => {
   console.log(`listening on port ${port} !!`);
 });
+
+app.use('/api/user' , userRouter)
+app.use('/api/auth', authRouter)
